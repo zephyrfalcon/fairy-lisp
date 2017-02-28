2344bf7946e526bed163230493ca1bccc7af29a7
Hans <zephyrfalcon@gmail.com>
2017-02-27 21:04:24 -0500

    added LispPair.Repr() (untested)

--- source/types.d
+++ source/types.d
@@ -1,5 +1,7 @@
 // types.d
 
+import std.array;
+
 abstract class LispObject {
     dstring Repr() { return "<undefined>"; }
 }
@@ -20,8 +22,24 @@ class LispPair : LispList {
     LispObject head;
     LispObject tail;
     override dstring Repr() {
-        // FIXME
-        return "(" ~ head.Repr() ~ " . " ~ tail.Repr() ~ ")";
+        //return "(" ~ head.Repr() ~ " . " ~ tail.Repr() ~ ")";
+        LispPair p = this;
+        dstring[] elems = [];
+        while (1) {
+            elems ~= p.head.Repr();
+            if (auto p2 = cast(LispPair)(p.tail)) {
+                // tail is also a pair; continue
+                p = p2;
+            } else if (cast(LispEmptyList)(p.tail)) {
+                // end of proper list has been reached; return repr
+                return "(" ~ join(elems, " ") ~ ")";
+            } else {
+                // improper list
+                dstring s = join(elems, " ");
+                s = s ~ " . " ~ p.tail.Repr();
+                return "(" ~ s ~ ")";
+            }
+        }
     }
 }
 
