bd6e41e6f7d48216129784a22e2863993b7e0e73
Hans <zephyrfalcon@gmail.com>
2017-03-19 10:22:08 -0400

    added special form: DEFINE (simple form only for now) (closes #21)

--- source/special.d
+++ source/special.d
@@ -1,15 +1,35 @@
 // special.d
 // Implementations of special forms.
 
+import errors;
 import interpreter;
+import stackframe;
 import types;
 
 LispObject sf_quote(Interpreter intp, LispEnvironment env, LispObject[] args) {
     return args[1];  // return the first argument as-is
 }
 
+// currently only: (DEFINE <name> <value>)
+LispObject sf_define(Interpreter intp, LispEnvironment env, LispObject[] args) {
+    if (auto sym = cast(LispSymbol) args[1]) {
+        StackFrame top = intp.callstack.Top();
+        if (top.evaluated.length == 0) {
+            auto sf = new StackFrame(args[2], top.env);
+            intp.callstack.Push(sf);
+            return null;  // evaluate via stack mechanism
+        } else {
+            // we've evaluated the value
+            env.Set(sym.value, top.evaluated[0]);
+            return top.evaluated[0];
+        }
+    } else 
+        throw new TypeError("DEFINE: name must be a symbol");
+}
+
 SpecialFormSig[dstring] GetSpecialForms() {
     SpecialFormSig[dstring] forms = [
+        "define": &sf_define,
         "quote": &sf_quote,
     ];
     return forms;
