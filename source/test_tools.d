07ecba57660d5252ac059342940c4d79aea9cbf4
Hans <zephyrfalcon@gmail.com>
2017-03-19 20:10:32 -0400

    test WrapExprsInDo() (closes #26)

--- source/test_tools.d
+++ source/test_tools.d
@@ -1,7 +1,9 @@
 // test_tools.d
 
+import interpreter;
 import tools;
 import tools_test;
+import types;
 
 unittest {
     AssertEquals(EscapeString("hello"d), `"hello"`d);
@@ -10,4 +12,15 @@ unittest {
 
 // test WrapExprsInDo()
 unittest {
+    auto intp = new Interpreter();
+
+    // multiple expressions get wrapped in a DO
+    auto results = intp.EvalString("3 4 5");
+    AssertEquals(results.length, 3);
+    auto expr = WrapExprsInDo(results);
+    AssertEquals(expr.Repr(), "(do 3 4 5)");
+
+    // a single expression doesn't get wrapped in a DO
+    expr = WrapExprsInDo([new LispSymbol("x")]);
+    AssertEquals(expr.Repr(), "x");
 }
