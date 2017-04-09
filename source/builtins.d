c7dafe4aa1856006ca0ed1b685046b4fbcb2a504
Hans <zephyrfalcon@gmail.com>
2017-04-09 13:18:56 -0400

    added ENV-GET, CURRENT-ENV (#53)

--- source/builtins.d
+++ source/builtins.d
@@ -71,6 +71,28 @@ LispObject b_print(Interpreter intp, LispEnvironment env, FunctionArgs fargs) {
     return last;
 }
 
+// (ENV-GET env name [default])
+LispObject b_env_get(Interpreter intp, LispEnvironment env, FunctionArgs fargs) {
+    if (auto this_env = cast(LispEnvironment) fargs.args[0]) {
+        if (auto name = cast(LispSymbol) fargs.args[1]) {
+            try {
+                LispObject result = this_env.Get(name.value);
+                return result;
+            } catch (EnvironmentKeyException e) {
+                if (fargs.rest_args.length > 0) 
+                    return fargs.rest_args[0];
+                throw e;
+            }
+        } else
+            throw new TypeError("ENV-GET: name must be a symbol");
+    } else 
+        throw new TypeError("ENV-GET: first argument must be an environment");
+}
+
+// return the current environment
+LispObject b_current_env(Interpreter intp, LispEnvironment env, FunctionArgs fargs) {
+    return env;
+}
 
 struct FI {
     BuiltinFunctionSig f;
@@ -82,6 +104,8 @@ FI[dstring] GetBuiltins() {
     FI[dstring] builtins = [
         "+": FI(&b_plus, 0),
         "addr": FI(&b_addr, 1),
+        "current-env": FI(&b_current_env, 0),
+        "env-get": FI(&b_env_get, 2),
         "eq?": FI(&b_eq, 2),
         "equal?": FI(&b_equal, 2),
         "print": FI(&b_print, 0),
