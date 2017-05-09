abcc10801c7695055452584fcfaf6bd2764fd260
Hans <zephyrfalcon@gmail.com>
2017-05-08 21:33:46 -0400

    added STRING-ENDS-WITH? (kinda related to #84)

--- source/b_string.d
+++ source/b_string.d
@@ -24,3 +24,14 @@ LispObject b_string_starts_with(Interpreter intp, LispEnvironment env, FunctionA
         throw new XTypeError("STRING-STARTS-WITH?", "string", fargs.args[0]);
 }
 
+LispObject b_string_ends_with(Interpreter intp, LispEnvironment env, FunctionArgs fargs) {
+    if (auto s = cast(LispString) fargs.args[0]) {
+        if (auto prefix = cast(LispString) fargs.args[1]) {
+            return endsWith(s.value, prefix.value) ? TRUE() : FALSE();
+        } else
+            throw new XTypeError("STRING-STARTS-WITH?", "string", fargs.args[1]);
+    } else
+        throw new XTypeError("STRING-STARTS-WITH?", "string", fargs.args[0]);
+}
+
+
