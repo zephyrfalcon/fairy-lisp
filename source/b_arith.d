0dabe427d99c9d00232676856321ecf715560288
Hans <zephyrfalcon@gmail.com>
2017-05-24 22:15:48 -0400

    fixed `=` operator (#93)

--- source/b_arith.d
+++ source/b_arith.d
@@ -12,6 +12,7 @@ import types;
 alias IntOpSig = int function(int, int);
 alias RealOpSig = real function(real, real);
 
+
 LispNumber binop(LispNumber a, LispNumber b, IntOpSig intop, RealOpSig realop) {
     if (auto ia = cast(LispInteger) a) {
         if (auto ib = cast(LispInteger) b) {
@@ -71,10 +72,41 @@ LispObject b_plus(Interpreter intp, LispEnvironment env, FunctionArgs fargs) {
 }
 */
 
-// XXX the code for =, <, etc, is extremely similar; we need a way to refactor
-// this
+// similar templates, but for comparing two numbers:
+
+alias CompIntOpSig = bool function(int, int);
+alias CompRealOpSig = bool function(real, real);
+
+LispBoolean cbinop(LispNumber a, LispNumber b, CompIntOpSig intop, CompRealOpSig realop) {
+    if (auto ia = cast(LispInteger) a) {
+        if (auto ib = cast(LispInteger) b) {
+            // integer addition
+            return intop(ia.value, ib.value) ? TRUE() : FALSE();
+        }
+    }
+    return realop(a.AsFloat(), b.AsFloat()) ? TRUE() : FALSE();
+}
+
+LispObject _cbinop_template(Interpreter intp, LispEnvironment env, FunctionArgs
+    fargs, dstring name, CompIntOpSig intop, CompRealOpSig realop) 
+{
+    if (auto a = cast(LispNumber) fargs.args[0]) {
+        if (auto b = cast(LispNumber) fargs.args[1]) {
+            return cbinop(a, b, intop, realop);
+        } else
+            throw new XTypeError(name, "number", fargs.args[1]);
+    } else
+        throw new XTypeError(name, "number", fargs.args[0]);
+}
 
 LispObject b_equals(Interpreter intp, LispEnvironment env, FunctionArgs fargs) {
+    return _cbinop_template(intp, env, fargs, "=", 
+            function(int x, int y) { return x == y; },
+            function(real x, real y) { return x == y; });
+}
+
+// OLD
+LispObject __b_equals(Interpreter intp, LispEnvironment env, FunctionArgs fargs) {
     if (auto n1 = cast(LispInteger) fargs.args[0]) {
         if (auto n2 = cast(LispInteger) fargs.args[1]) {
             return (n1.value == n2.value) ? TRUE() : FALSE();
@@ -84,6 +116,7 @@ LispObject b_equals(Interpreter intp, LispEnvironment env, FunctionArgs fargs) {
         throw new TypeError("number expected");
 }
 
+// FIXME
 LispObject b_less_than(Interpreter intp, LispEnvironment env, FunctionArgs fargs) {
     if (auto n1 = cast(LispInteger) fargs.args[0]) {
         if (auto n2 = cast(LispInteger) fargs.args[1]) {
@@ -94,6 +127,7 @@ LispObject b_less_than(Interpreter intp, LispEnvironment env, FunctionArgs fargs
         throw new TypeError("number expected");
 }
 
+// FIXME
 LispObject b_greater_than(Interpreter intp, LispEnvironment env, FunctionArgs fargs) {
     if (auto n1 = cast(LispInteger) fargs.args[0]) {
         if (auto n2 = cast(LispInteger) fargs.args[1]) {
